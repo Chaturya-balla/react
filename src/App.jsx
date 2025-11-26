be65e4f0bf3d888fe013c74faa5e272df8242523
Chaturya-balla <ballachaturya@gmail.com>
2025-11-26 20:14:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+  <div>
+    <h1 className='text-danger'>Welcome to react</h1>
+    <h1 className="bg-success">This is react component</h1>
+  </div>
+  );
+}
+
+export default App
